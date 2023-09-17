c550f28c33d0a06067bf5b8321140078a4b85e13
raulc <raul.cardoso@outlook.com.br>
2023-09-17 01:53:49 -0300

    Removidas imports desnecessários

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Formulario from './components/Formulario.vue';
-import ListaProdutos from './components/ListaProdutos.vue';
-
 
 export default defineComponent({
   name: 'App',
