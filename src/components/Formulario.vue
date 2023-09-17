8d47487bb131e318ab65155b9b067cbc6efae863
raulc <raul.cardoso@outlook.com.br>
2023-09-17 00:57:25 -0300

    Adicionado bootstrap; Excluída pagina de HelloWord; Criada pagina do formulário de cadastro de produtos;

---
+++ src/components/Formulario.vue
@@ -0,0 +1,33 @@
+<template >
+    <div class="container w-50">
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome do produto</label>
+                <input type="text" class="form-control" id="name" placeholder="Nome do Produto">
+            </div> 
+            <div class="mb-3">
+                <label for="value" class="form-label">Valor do produto</label>
+                <input type="number" class="form-control" id="value" placeholder="Valor do Produto">
+            </div> 
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrição do produto</label>
+                <textarea class="form-control" id="description" placeholder="Descrição do Produto" rows="3"></textarea>
+            </div>
+            
+            <div className="text-center m-2">
+                <Button type="button" class="btn btn-success mx-2">Salvar</Button>
+                <Button type="button" class="btn btn-primary">Voltar</Button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'formularioProduto',
+    
+})
+
+</script>
